d547f67c23df071318472a6abb033833245f0cf3
Kasper Schøn Henriksen <schonsteren@gmail.com>
2019-09-25 11:42:04 +0200

    - Added LaserSensor

--- Assets/Scripts/LaserSensor.cs
+++ Assets/Scripts/LaserSensor.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserSensor : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class LaserSensor : MonoBehaviour{
+    public float rotationSpeed = 20f;
+    void Start(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        RotateSensor();
+        LaserRaycast(); 
+    }
+
+
+    private void RotateSensor(){
+        transform.Rotate(Vector3.right*rotationSpeed,Space.Self);
+    }
+
+    private void LaserRaycast(){
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.up),out hit, 100f)){
+            print("Distance: "+hit.distance);
+            Debug.DrawRay(transform.position,transform.TransformDirection(Vector3.up)*100,Color.red);
+        }else{
+            Debug.DrawRay(transform.position,transform.TransformDirection(Vector3.up)*100,Color.yellow);
+        }
     }
 }
