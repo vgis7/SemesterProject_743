928e51f9e9512dfc956daff62ec5af805bde3b08
LynzZen <mathias.s.lynge@gmail.com>
2019-09-25 12:39:55 +0200

    Added canvas

---
+++ Assets/Scripts/Window_Graph.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Window_Graph : MonoBehaviour
+{
+    LaserSensor LS;
+    private RectTransform graphContainer;
+    [SerializeField] private Sprite circleSprite;
+    private void Awake() {
+        graphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
+
+    }
+
+    private void CreatePoint(Vector2 Position){
+        GameObject gameObject = new GameObject("point", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = Position;
+        rectTransform.sizeDelta = new Vector2(11,11);
+        rectTransform.anchorMin = new Vector2(0,0);
+        rectTransform.anchorMax = new Vector2(0,0);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        LS = GetComponent<LaserSensor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreatePoint((LS.hitposition[0]));
+    }
+}
