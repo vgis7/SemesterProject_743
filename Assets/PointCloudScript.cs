ddea324a2428d6f4cbbe31522ac47eaa64348827
Kasper Schøn Henriksen <schonsteren@gmail.com>
2019-09-30 17:47:16 +0200

    Pico Flexx
    Added:
    - The sensor
    - PointCloud
    - Arrays of Rays and Points
    - Rays send hitpositions to Points

---
+++ Assets/PointCloudScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCloudScript : MonoBehaviour{
+    Transform pointsContainer;
+    public GameObject pointPrefab;
+    GameObject[,] pointArray = new GameObject[Screen.width/10,Screen.height/10];
+
+    void Start(){
+        pointsContainer = this.transform.Find("PointsContainer");
+
+        int columnAmount = pointArray.GetLength(0); //X
+        int rowAmount = pointArray.GetLength(1); //Y
+
+        for(int y = 0;y<rowAmount;y++){
+            for(int x = 0; x<columnAmount;x++){
+                GameObject newPoint = Instantiate(pointPrefab,pointsContainer);
+                pointArray[x,y]=newPoint;
+            }
+        }
+    }
+    
+    void Update(){
+        
+    }
+
+    public void SetPointPosition(Vector2 rayID,Vector3 hitPosition){
+        int x = (int) rayID.x;
+        int y = (int) rayID.y;
+        pointArray[x,y].transform.position = pointsContainer.position+hitPosition;
+        //print("RayID: "+rayID+" "+"HitPosition: "+hitPosition);
+    }
+}
---
+++ Assets/Scripts/PicoFlexxScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PicoFlexxScript : MonoBehaviour{
+
+    Camera picoFlexCamera;
+    public PointCloudScript pointCloud;
+    Ray[,] rayArray = new Ray[Screen.width/10,Screen.height/10];
+
+    void Start(){
+        picoFlexCamera = this.transform.Find("Camera").GetComponent<Camera>();
+
+    }
+
+    void Update(){
+        CreateRays();
+        CastRays();
+    }
+    
+    private void CreateRays(){
+        int columnAmount = rayArray.GetLength(0); //X
+        int rowAmount = rayArray.GetLength(1); //Y
+
+        for(int y = 0;y<rowAmount;y++){
+            for(int x = 0; x<columnAmount;x++){
+                Ray newRay = picoFlexCamera.ScreenPointToRay(new Vector3(x*10,y*10,0));
+                rayArray[x,y]= newRay;
+            }
+        }
+    }
+
+    private void CastRays(){
+        int columnAmount = rayArray.GetLength(0); //X
+        int rowAmount = rayArray.GetLength(1); //Y
+        for(int y = 0;y<rowAmount;y++){
+            for(int x = 0; x<columnAmount;x++){
+                Ray currentRay = rayArray[x,y];
+                Debug.DrawRay(currentRay.origin,currentRay.direction,Color.red);
+
+                RaycastHit hit;
+                if(Physics.Raycast(currentRay,out hit)){
+                    Vector3 hitPosition = hit.point;
+
+                    Vector2 rayID = new Vector2(x,y);
+                    pointCloud.SetPointPosition(rayID,hitPosition);
+                }
+            }
+        }
+    }
+}
