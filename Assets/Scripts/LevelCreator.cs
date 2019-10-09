d20457928c8cc9f7c29eb6df15b4b70f83a7da01
Kasper Henriksen <schonsteren@gmail.com>
2019-10-10 00:18:26 +0200

    Small Update For pipe creation

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -6,12 +6,26 @@ public class LevelCreator : MonoBehaviour{
     public GameObject pipeStraight, pipeBend;
 
     void Start(){
-        for(int i = 0; i<4;i++){
-            int random = Random.Range(0,1);
+        Vector3 createNextObjectAtVector = new Vector3(0,0,3);
 
-            GameObject newPipe = Instantiate(pipeStraight);
-            newPipe.transform.position = new Vector3(0,0,3f*i);
-            newPipe.transform.Rotate(new Vector3(-90,0,0));
+        for(int i = 0; i<3;i++){
+            int random = Random.Range(0,2);
+            print(random);
+            if(random == 0){
+                GameObject newPipe = Instantiate(pipeStraight);
+                newPipe.transform.Rotate(new Vector3(-90, 0, 0));
+                newPipe.transform.position = createNextObjectAtVector;
+                createNextObjectAtVector += new Vector3(0,0,3);
+            }
+            if(random == 1){
+                GameObject newPipe = Instantiate(pipeBend);
+                newPipe.transform.Rotate(new Vector3(0, 0, 0));
+                newPipe.transform.position = createNextObjectAtVector;
+                createNextObjectAtVector += new Vector3(0, 0, 1.5f);
+            }
+            //GameObject newPipe = Instantiate(pipeStraight);
+            //newPipe.transform.position = new Vector3(0,0,3f*i);
+            //newPipe.transform.Rotate(new Vector3(-90,0,0));
         }
     }
 
