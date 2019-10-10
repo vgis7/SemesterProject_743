98fd4ef58805e4071d62e182fb394390403d7b70
Kasper Henriksen <schonsteren@gmail.com>
2019-10-10 23:34:06 +0200

    Spline Update + Straight Pipe Prefab
    - Create spline points for straight pipe prefab

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -4,28 +4,26 @@ using UnityEngine;
 
 public class LevelCreator : MonoBehaviour{
     public GameObject pipeStraight, pipeBend;
+    public GameObject splineRenderer;
 
     void Start(){
         Vector3 createNextObjectAtVector = new Vector3(0,0,3);
 
-        for(int i = 0; i<3;i++){
-            int random = Random.Range(0,2);
-            print(random);
+        for(int i = 0; i<5;i++){
+            int random = Random.Range(0,0);
             if(random == 0){
                 GameObject newPipe = Instantiate(pipeStraight);
-                newPipe.transform.Rotate(new Vector3(-90, 0, 0));
+                newPipe.transform.SetParent(this.transform.Find("ObjectContainer"));
+                //newPipe.transform.Rotate(new Vector3(-90, 0, 0));
                 newPipe.transform.position = createNextObjectAtVector;
                 createNextObjectAtVector += new Vector3(0,0,3);
             }
-            if(random == 1){
+            /*if(random == 1){
                 GameObject newPipe = Instantiate(pipeBend);
                 newPipe.transform.Rotate(new Vector3(0, 0, 0));
                 newPipe.transform.position = createNextObjectAtVector;
                 createNextObjectAtVector += new Vector3(0, 0, 1.5f);
-            }
-            //GameObject newPipe = Instantiate(pipeStraight);
-            //newPipe.transform.position = new Vector3(0,0,3f*i);
-            //newPipe.transform.Rotate(new Vector3(-90,0,0));
+            }*/
         }
     }
 
---
+++ Assets/SplineCreator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineCreator : MonoBehaviour{
+
+    private LineRenderer lineRenderer;
+    public GameObject objectContainer;
+
+    void Start(){
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update(){
+        int pointPerObject = 10;
+        lineRenderer.positionCount = objectContainer.transform.childCount* pointPerObject;
+
+        //Get Objects from object container
+        GameObject[] objects = new GameObject[objectContainer.transform.childCount];
+        for (int i = 0; i < objectContainer.transform.childCount; i++) {
+            objects[i] = objectContainer.transform.GetChild(i).gameObject;
+        }
+
+        //CURVES
+        int pointCounter = 0;
+        for (int i = 0; i < objectContainer.transform.childCount; i++) {
+            Vector3 startPosition = objects[i].transform.Find("Start").transform.position;
+            Vector3 middlePosition = objects[i].transform.Find("Middle").transform.position;
+            Vector3 endPosition = objects[i].transform.Find("End").transform.position;
+
+            for(int j = 0; j < pointPerObject; j++){
+                float t = pointCounter / (lineRenderer.positionCount - 1.0f);
+                Vector3 position = (1.0f - t) * (1.0f - t) * startPosition + 2.0f * (1.0f - t) * t * middlePosition + t * t * endPosition;
+                lineRenderer.SetPosition(pointCounter, position);
+                pointCounter++;
+            }
+        }
+    }
+}
