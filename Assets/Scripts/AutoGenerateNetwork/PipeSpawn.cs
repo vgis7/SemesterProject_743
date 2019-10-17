f448a54e8c74b3d11bb5513d33d43b241c3d442f
LynzZen <mathias.s.lynge@gmail.com>
2019-10-17 12:27:27 +0200

    Added automatic pipe network creation

---
+++ Assets/Scripts/AutoGenerateNetwork/PipeSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawn : MonoBehaviour
+{
+ 
+    public GameObject pipeToCreate;
+ void SpawnPipes(int amount) {
+     GameObject newPipe = Instantiate(pipeToCreate);
+    
+     for (int i = 0; i < amount; i++)
+     {
+         GameObject tempPipe = SpawnObject(newPipe);
+         newPipe=tempPipe;
+     }
+ }  
+ GameObject SpawnObject(GameObject preObject) {
+     Vector3 startPosition = preObject.transform.Find("Start").position;  
+     GameObject newPipe = Instantiate(pipeToCreate);
+     newPipe.transform.position =  startPosition;
+     return newPipe; 
+ }
+    void Start()
+    {
+        SpawnPipes(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
