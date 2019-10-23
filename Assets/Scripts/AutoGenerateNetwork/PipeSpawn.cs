fa647c012825d7af4d889df93b90dddcd8f596ae
LynzZen <mathias.s.lynge@gmail.com>
2019-10-23 09:41:47 +0200

    New pipes

--- Assets/Scripts/AutoGenerateNetwork/PipeSpawn.cs
+++ Assets/Scripts/AutoGenerateNetwork/PipeSpawn.cs
@@ -5,83 +5,130 @@ using UnityEngine;
 public class PipeSpawn : MonoBehaviour
 {
     int preState = 0;
-    public GameObject StraightPipe;
-    public GameObject Bend15Pipe;
 
-
-    public GameObject Bend30Pipe,Bend45Pipe;
- void SpawnPipes(int amount) {
-     GameObject newPipe = Instantiate(StraightPipe);
-     newPipe.transform.name = "PipeStraight";
-     for (int i = 0; i < amount; i++)
-     {
-         GameObject tempPipe = SpawnObject2(newPipe);
-         newPipe=tempPipe;
-     }
- }  
+    public GameObject StraightPipe,Bend15Pipe,Bend30Pipe,Bend30PipeR,Bend45Pipe;
+    
+    void SpawnPipes(int amount) {
+        GameObject newPipe = Instantiate(StraightPipe);
+        newPipe.transform.name = "PipeStraight";
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject tempPipe = SpawnObject2(newPipe);
+            newPipe=tempPipe;
+        }
+    }  
      
      GameObject SpawnObject2(GameObject preObject){
         //Get Head position from point
         Vector3 startPosition = preObject.transform.Find("Head").position;
         Vector3 WorldRotation = preObject.transform.rotation.eulerAngles;
 
+        
+        
+        ////////////////////
+        /// Instantiate Pipes
+        ////////////////////
+
         GameObject newPipe = new GameObject();
         int number;
         if(preState == 1){
-            number = Random.Range(1,5);
+            number = Random.Range(1,6);
         } else {
             number = 1;
         }
         preState = number;
         switch(number){
+            //Pipe Straight
             case 1:
                 newPipe = Instantiate(StraightPipe);
                 newPipe.transform.position = startPosition;
                 newPipe.transform.name = "PipeStraight";
                 break;
+            //Pipe 15
             case 2:
                 newPipe = Instantiate(Bend15Pipe);
                 newPipe.transform.position = startPosition;
                 newPipe.transform.name = "PipeBend15";
                 break;
             case 3:
+                newPipe = Instantiate(Bend15Pipe);
+                newPipe.transform.position = startPosition;
+                newPipe.transform.name = "PipeBend15R";
+                break;
+            //Pipe 30
+            case 4:
                 newPipe = Instantiate(Bend30Pipe);
                 newPipe.transform.position = startPosition;
                 newPipe.transform.name = "PipeBend30";
                 break;
-            case 4:
+            case 5:
+                newPipe = Instantiate(Bend30PipeR);
+                newPipe.transform.position = startPosition;
+                newPipe.transform.name = "PipeBend30R";
+                break;
+            //Pipe 45
+            case 6:
                 newPipe = Instantiate(Bend45Pipe);
                 newPipe.transform.position = startPosition;
                 newPipe.transform.name = "PipeBend45";
                 break;
+            case 7:
+                newPipe = Instantiate(Bend45Pipe);
+                newPipe.transform.position = startPosition;
+                newPipe.transform.name = "PipeBend45R";
+                break;
         }
 
-        int rotationAroundZ = 0;
-        int randomNumber = Random.Range(0,2);
-        if(randomNumber == 0){
-            rotationAroundZ = 0;
-        }else{
-            rotationAroundZ = -180;
-        }
+        
+        ////////////////////
+        /// Rotatate Pipes
+        ////////////////////
 
-         if(preObject.transform.name == "PipeBend15"){
-                    newPipe.transform.Rotate(WorldRotation + new Vector3(0,15,rotationAroundZ), Space.World);
-         }
+        //Pipe 15
+        if(preObject.transform.name == "PipeBend15"){
+            newPipe.transform.Rotate(WorldRotation + new Vector3(0,15,0), Space.World);
+        }else if(preObject.transform.name == "PipeBend15R"){
+            newPipe.transform.Rotate(WorldRotation + new Vector3(0,-15,0), Space.World);
+        }
+        //Pipe 30
         else if(preObject.transform.name == "PipeBend30"){
-                    newPipe.transform.Rotate(WorldRotation + new Vector3(0,30,rotationAroundZ), Space.World);
+            newPipe.transform.Rotate(WorldRotation + new Vector3(0,30,0), Space.World);
+        }else if(preObject.transform.name == "PipeBend30R"){
+            newPipe.transform.Rotate(WorldRotation + new Vector3(0,-30,0), Space.World);
+        }
+        //Pipe 45
+        else if(preObject.transform.name == "PipeBend45"){
+            newPipe.transform.Rotate(WorldRotation + new Vector3(0,45,0), Space.World);
+        }else if(preObject.transform.name == "PipeBend45R"){
+            newPipe.transform.Rotate(WorldRotation + new Vector3(0,-45,0), Space.World);
+        }
+        //Pipe Straight
+        else{
+            newPipe.transform.Rotate(WorldRotation, Space.World);
+        }
+
+        /*
+        if(preObject.transform.name == "PipeBend15"){
+                newPipe.transform.Rotate(WorldRotation + new Vector3(0,15,0), Space.World);
+        }
+        else if(preObject.transform.name == "PipeBend30"){
+                    newPipe.transform.Rotate(WorldRotation + new Vector3(0,30,0), Space.World);
         }
         else if(preObject.transform.name == "PipeBend45"){
-                    newPipe.transform.Rotate(WorldRotation + new Vector3(0,45,rotationAroundZ), Space.World);
-         }       
-         else {
-            newPipe.transform.Rotate(new Vector3 (0,WorldRotation.y*-1,0), Space.World);
-         }
+                    newPipe.transform.Rotate(WorldRotation + new Vector3(0,45,0), Space.World);
+            }    
+        else if(preObject.transform.name == "PipeBend30R"){
+                    newPipe.transform.Rotate(WorldRotation + new Vector3(0,-30,0), Space.World);
+        }       
+        else {
+        newPipe.transform.Rotate(WorldRotation, Space.World);
+        }*/
         return newPipe;
     }
 
     void Start()
     {
-        SpawnPipes(10);
+        SpawnPipes(20);
     }
 
     // Update is called once per frame
